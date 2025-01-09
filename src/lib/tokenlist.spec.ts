faa344a42afd077027f9277af2bbac088a3e3ce8
Viet Lai <dev.vietlt@gmail.com>
2025-01-09 23:29:34 +0700

    update test

--- src/lib/tokenlist.spec.ts
+++ src/lib/tokenlist.spec.ts
@@ -2,58 +2,7 @@ import fs from 'fs';
 
 import test from 'ava';
 
-import {
-  CLUSTER_SLUGS,
-  ENV,
-  Strategy,
-  TokenInfo,
-  TokenListProvider,
-} from './tokenlist';
-
-test('Token list is filterable by a tag', async (t) => {
-  const list = (await new TokenListProvider().resolve(Strategy.GitHub))
-    .filterByChainId(ENV.PulseChain)
-    .filterByTag('nft')
-    .getList();
-
-  t.false(list.some((item) => item.symbol === 'SOL'));
-});
-
-test('Token list can exclude by a tag', async (t) => {
-  const list = (await new TokenListProvider().resolve(Strategy.GitHub))
-    .filterByChainId(ENV.PulseChain)
-    .excludeByTag('nft')
-    .getList();
-
-  t.false(list.some((item) => item.tags === ['nft']));
-});
-
-test('Token list can exclude by a chain', async (t) => {
-  const list = (await new TokenListProvider().resolve(Strategy.GitHub))
-    .excludeByChainId(ENV.PulseChain)
-    .getList();
-
-  t.false(list.some((item) => item.chainId === ENV.PulseChain));
-});
-
-test('Token list returns new object upon filter', async (t) => {
-  const list = await new TokenListProvider().resolve(Strategy.GitHub);
-  const filtered = list.filterByChainId(ENV.PulseChain);
-  t.true(list !== filtered);
-  t.true(list.getList().length !== filtered.getList().length);
-});
-
-test('Token list throws error when calling filterByClusterSlug with slug that does not exist', async (t) => {
-  const list = await new TokenListProvider().resolve(Strategy.GitHub);
-  const error = await t.throwsAsync(
-    async () => list.filterByClusterSlug('whoop'),
-    { instanceOf: Error }
-  );
-  t.is(
-    error.message,
-    `Unknown slug: whoop, please use one of ${Object.keys(CLUSTER_SLUGS)}`
-  );
-});
+import { Strategy, TokenInfo, TokenListProvider } from './tokenlist';
 
 test('Token list is a valid json', async (t) => {
   t.notThrows(() => {
@@ -66,16 +15,13 @@ test('Token list is a valid json', async (t) => {
 
 test('Token list does not have duplicate entries', async (t) => {
   const list = await new TokenListProvider().resolve(Strategy.GitHub);
-  list
-    .filterByChainId(ENV.PulseChain)
-    .getList()
-    .reduce((agg, item) => {
-      if (agg.has(item.address)) {
-        console.log(item.address);
-      }
+  list.getList().reduce((agg, item) => {
+    if (agg.has(item.address)) {
+      console.log(item.address);
+    }
 
-      t.false(agg.has(item.address));
-      agg.set(item.address, item);
-      return agg;
-    }, new Map<string, TokenInfo>());
+    t.false(agg.has(item.address));
+    agg.set(item.address, item);
+    return agg;
+  }, new Map<string, TokenInfo>());
 });
